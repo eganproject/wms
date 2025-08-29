9e902f0c2506766bb1d6559bc9374998c8e38e87
Ega <eganugrahaid@gmail.com>
2025-08-29 09:21:06 +0700

    membuat migrasi untuk kebutuhan stok in dan inventory

---
+++ database/migrations/2025_08_29_021813_create_stock_in_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_in_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->date('date');
+            $table->foreignId('warehouse_id')->constrained('warehouses')->onDelete('cascade');
+            $table->enum('status', ['requested', 'shipped', 'completed', 'rejected'])->default('requested');
+            $table->mediumText('description')->nullable();
+            $table->bigInteger('requested_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->bigInteger('approved_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->date('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_in_orders');
+    }
+};
---
+++ database/migrations/2025_08_29_021903_create_stock_movements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_movements', function (Blueprint $table) {
+            $table->id();
+            $table->integer('item_id');
+            $table->integer('warehouse_id');
+            $table->decimal('quantity', 20, 2)->default(0);
+            $table->decimal('koli', 20, 2)->default(0);
+            $table->enum('type', ['stock_in', 'stock_out', 'transfer_in', 'transfer_out', 'adjustment']);
+            $table->mediumText('description')->nullable();
+            $table->bigInteger('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->bigInteger('reference_id')->comment('ID dari dokumen sumber (misal: id dari stock_in_orders atau transfer_requests)')->nullable();
+            $table->enum('reference_type', ['stock_in_orders', 'transfer_requests', 'stock_out'])->comment('Tipe dokumen sumber (misal: stock_in_orders atau transfer_requests)');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_movements');
+    }
+};
