bf35c303754b1b9c7ed2866bb59e791f90e2ab95
Ega <eganugrahaid@gmail.com>
2025-09-04 14:32:28 +0700

    inisiasi stok opname

--- app/Http/Controllers/Admin/ManajemenStok/StockOpnameController.php
+++ app/Http/Controllers/Admin/ManajemenStok/StockOpnameController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\ManajemenStok;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class StockOpnameController extends Controller
